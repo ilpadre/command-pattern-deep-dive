da1adc2e1f9ca2886e2cb872e41af99a7bc68101
ilpadre <ilpadre1953@gmail.com>
2019-06-09 22:09:23 -0400

    Initial commit

---
+++ donation-manager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace donation_manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
