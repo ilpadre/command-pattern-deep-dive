bb05a4ffb27306528bb1de46da3c7bdd77e578e3
ilpadre <ilpadre1953@gmail.com>
2019-06-09 22:18:46 -0400

    Add code

--- reference-implementation/Program.cs
+++ reference-implementation/Program.cs
@@ -2,11 +2,93 @@
 
 namespace reference_implementation
 {
-    class Program
+    /// <summary>
+    /// MainApp startup class for Reference
+    /// Command Design Pattern.
+    /// </summary>
+    class MainApp
+
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // Create receiver, command, and invoker
+
+            Receiver receiver = new Receiver();
+            Command command = new ConcreteCommand(receiver);
+            Invoker invoker = new Invoker();
+
+            // Set and execute command
+
+            invoker.SetCommand(command);
+            invoker.ExecuteCommand();
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Command' abstract class
+    /// </summary>
+    abstract class Command
+    {
+        protected Receiver receiver;
+
+        public Command(Receiver receiver)
+        {
+            this.receiver = receiver;
+        }
+
+        public abstract void Execute();
+    }
+
+    /// <summary>
+    /// The 'ConcreteCommand' class
+    /// </summary>
+    class ConcreteCommand : Command
+    {
+
+        public ConcreteCommand(Receiver receiver) :
+            base(receiver)
+        {
+        }
+
+        public override void Execute()
+        {
+            receiver.Action();
+        }
+    }
+
+    /// <summary>
+    /// The 'Receiver' class
+    /// </summary>
+    class Receiver
+    {
+        public void Action()
+        {
+            Console.WriteLine("Called Receiver.Action()");
+        }
+    }
+
+    /// <summary>
+    /// The 'Invoker' class
+    /// </summary>
+    class Invoker
+    {
+        private Command _command;
+
+        public void SetCommand(Command command)
+        {
+            this._command = command;
+        }
+
+        public void ExecuteCommand()
+        {
+            _command.Execute();
         }
     }
 }
